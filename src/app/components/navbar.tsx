894ad5e22156cdf64bdf3cc3a51d3ec508b9295c
Jeovane venâncio <40653032+JeovaneVenic@users.noreply.github.com>
2024-12-06 17:27:03 -0300

    Navbar mobile acrescentada e Responsividade corrigida.

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,48 +2,67 @@
 
 import React, { useState } from 'react';
 import Link from 'next/link';
-import ModalLogin from '../components/ModalLogin/ModalLogin';
+import ModalLogin from '../components/ModalLogin/ModalLogin'; // Certifique-se de ajustar o caminho conforme necessário
 import estilos from '../Styles/Navbar.module.css';
+import BarraDeNavegacaoMobile from '../components/NavbarMobile'; // Certifique-se de que o caminho está correto
 
 const BarraDeNavegacao: React.FC = () => {
   const [modalAberto, setModalAberto] = useState(false);
   const [mostrarCadastro, setMostrarCadastro] = useState(false);
+  const [menuMobileAberto, setMenuMobileAberto] = useState(false);
 
   const abrirModal = () => setModalAberto(true);
   const fecharModal = () => setModalAberto(false);
   const mostrarTelaCadastro = () => setMostrarCadastro(true);
   const mostrarTelaLogin = () => setMostrarCadastro(false);
+  const alternarMenuMobile = () => setMenuMobileAberto(!menuMobileAberto);
+  const fecharMenuMobile = () => setMenuMobileAberto(false);
 
   return (
     <div>
       <nav className={estilos.navbar}>
-        <div className={estilos.logo}>
-          <img src="/logo.png" alt="Logo" className={estilos.logoImagem} />
+        <div className={estilos.logoContainer}>
+          <Link href="/" passHref>
+            <div className={estilos.logo}>
+              <img src="/logo.png" alt="Logo" className={estilos.logoImagem} />
+            </div>
+          </Link>
         </div>
-        <ul className={estilos.menu}>
-          <li>
-            <Link href="/agendamento">Agendamento</Link>
-          </li>
-          <li>
-            <Link href="/cortes">Cortes</Link>
-          </li>
-          <li>
-            <Link href="/">Início</Link>
-          </li>
-          <li>
-            <Link href="/sobre">Sobre</Link>
-          </li>
-          <li>
-            <Link href="/localizacao">Localização</Link>
-          </li>
-        </ul>
-        <div className={estilos.contato}>
-          <button className={estilos.botaoContato} onClick={abrirModal}>
-            Entrar / Cadastre-se
-          </button>
+        <div className={estilos.menuContainer}>
+          <ul className={estilos.menu}>
+            <li>
+              <Link href="/agendamento">Agendamento</Link>
+            </li>
+            <li>
+              <Link href="/cortes">Cortes</Link>
+            </li>
+            <li>
+              <Link href="/">Início</Link>
+            </li>
+            <li>
+              <Link href="/sobre">Sobre</Link>
+            </li>
+            <li>
+              <Link href="/localizacao">Localização</Link>
+            </li>
+          </ul>
+        </div>
+        <div className={estilos.contatoContainer}>
+          <div className={estilos.contato}>
+            <button className={estilos.botaoContato} onClick={abrirModal}>
+              Entrar / Cadastre-se
+            </button>
+          </div>
+          <div className={estilos.menuBtn} onClick={alternarMenuMobile}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </div>
       </nav>
 
+      {menuMobileAberto && <BarraDeNavegacaoMobile fecharMenuMobile={fecharMenuMobile} />}
+      
       <ModalLogin 
         isOpen={modalAberto} 
         onClose={fecharModal} 
